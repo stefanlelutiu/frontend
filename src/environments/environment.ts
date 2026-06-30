32713a38b7afc557b609b08949a66e49e1ab6454
leluts <stefan.lelutiu@msg.group>
2026-06-30 16:26:01 +0300

    Changed azure workflow

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://backend.lemoncliff-ed9521c1.westus2.azurecontainerapps.io:8080/api'
+  apiUrl: 'https://backend.lemoncliff-ed9521c1.westus2.azurecontainerapps.io/api'
 };
